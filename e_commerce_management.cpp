d9a017f9f7cb5cb9776818a58257f02815923d8a
Fazle-Rabbi-Rounok <149162051+Fazle-Rabbi-Rounok@users.noreply.github.com>
2024-10-01 04:43:28 +0600

    Create e_commerce_management

---
+++ e_commerce_management.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Rshop
+{
+    string log_in = "admin";
+    string log_pass = "admin123";
+    void remove_seller()
+    {
+    }
+
+protected:
+    void add_product()
+    {
+    }
+    void stock_manage()
+    {
+    }
+    void price_update()
+    {
+    }
+    void profit()
+    {
+    }
+    void remove_product()
+    {
+    }
+};
+class seller : public Rshop
+{
+    string log_in;
+    string log_pass;
+};
+class customer
+{
+    string log_in;
+    string log_pass;
+
+public:
+    void search_product()
+    {
+    }
+    void buy_product()
+    {
+    }
+    void add_to_cart()
+    {
+    }
+    void payment()
+    {
+    }
+    void order_details()
+    {
+    }
+};
+int main()
+{
+}
