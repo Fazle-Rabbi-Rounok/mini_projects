a8db6b585ca09e2bedf2286cc0ac259405e75ce0
Fazle-Rabbi-Rounok <frrounok2021@gmail.com>
2024-10-01 19:38:12 +0600

    ff

--- e_commerce_management.cpp
+++ e_commerce_management.cpp
@@ -1,13 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-class Rshop
-{
-public:
-    string user_name = "admin";
-    string log_pass = "admin123";
-};
-class seller : public Rshop
+class seller
 {
 public:
     int id;
@@ -31,6 +24,8 @@ public:
     {
     }
 };
+vector<seller> seller_list;
+
 class customer
 {
 public:
@@ -55,90 +50,101 @@ public:
     {
     }
 };
-class purchase;
-class products;
-vector<seller> seller_list;
 vector<customer> customer_list;
+
+class purchase
+{
+};
 vector<purchase> purchase_hstry;
+
+class products
+{
+};
 vector<products> all_prod;
-void view_seller()
+class Rshop
 {
-    for (auto &a : seller_list)
+    string user_name = "admin";
+    string log_pass = "admin123";
+
+public:
+    void view_seller()
     {
-        cout << "name: " << a.name << "\nid: " << a.id << "\n";
-    }
-}
-void view_customers()
-{
-    for (auto &a : customer_list)
-        cout << "name: " << a.name << "\nid: " << a.id << "\n";
-}
-void ban_seller()
-{
-    cout << "es";
-}
-void dashboard()
-{
-    Rshop obj;
-    cout << "welcome to admin dashboar:  \n\nplease choose any option given below: \n1.view sellers\n2.view customers\n3. ban someone\n4.exit\nchoose: ";
-    int choose;
-    cin >> choose;
-    while (choose != 4)
-    {
-        switch (choose)
+        for (auto &a : seller_list)
         {
-        case 1:
-            view_seller();
-            break;
-        case 2:
-            view_customers();
-            break;
-        case 3:
-            ban_();
-            break;
-        default:
-            cout << "invalid input\n ";
-            break;
+            cout << "name: " << a.name << "\nid: " << a.id << "\n";
         }
-        cout << "1.view sellers\n2.view customers\n3. ban someone\n4.exit\nchoose: ";
-        cin >> choose;
     }
-}
-void ban_()
-{
-    cout << "1.ban a seller\n2.ban a customer\n3.return\nchoose: ";
-    int choose;
-    cin >> choose;
-    while (choose < 1 || choose > 3)
+    void view_customers()
     {
-        cout << "invalid input\nchoose: ";
-        cin >> choose;
+        for (auto &a : customer_list)
+            cout << "name: " << a.name << "\nid: " << a.id << "\n";
     }
-    if (choose == 1)
-        ban_seller();
-    else if (choose == 2)
-        ban_customer();
-}
-void ban_customer()
-{
-}
-void admin_dashboard()
-{
-    Rshop obj;
-    string user, logpass;
-    cin.ignore();
-    cout << "please log in:\nenter user name: ";
-    getline(cin, user);
-    cout << "enter password: ";
-    getline(cin, logpass);
-    if (obj.user_name == user && obj.log_pass == logpass)
-        dashboard();
-    else
-        cout << "wrong username or password\n";
-}
+    void admin_dashboard()
+    {
+        string user, logpass;
+        cin.ignore();
+        cout << "please log in:\nenter user name: ";
+        getline(cin, user);
+        cout << "enter password: ";
+        getline(cin, logpass);
+        if (user_name == user && log_pass == logpass)
+            dashboard();
+        else
+            cout << "wrong username or password\n";
+    }
+    void dashboard()
+    {
+        cout << "welcome to admin dashboar:  \n\nplease choose any option given below: \n1.view sellers\n2.view customers\n3. ban someone\n4.exit\nchoose: ";
+        int choose;
+        cin >> choose;
+        while (choose != 4)
+        {
+            switch (choose)
+            {
+            case 1:
+                view_seller();
+                break;
+            case 2:
+                view_customers();
+                break;
+            case 3:
+                ban_();
+                break;
+            default:
+                cout << "invalid input\n ";
+                break;
+            }
+            cout << "1.view sellers\n2.view customers\n3. ban someone\n4.exit\nchoose: ";
+            cin >> choose;
+        }
+    }
+    void ban_seller()
+    {
+        cout << "es\n";
+    }
+    void ban_()
+    {
+        cout << "1.ban a seller\n2.ban a customer\n3.return\nchoose: ";
+        int choose;
+        cin >> choose;
+        while (choose < 1 || choose > 3)
+        {
+            cout << "invalid input\nchoose: ";
+            cin >> choose;
+        }
+        if (choose == 1)
+            ban_seller();
+        else if (choose == 2)
+            ban_customer();
+    }
+    void ban_customer()
+    {
+    }
+};
+
 int main()
 {
-    Rshop admin_obj;
+    Rshop obj;
     cout << "1.log in as admin\n2.log in as a seller\n3.log in as customer\nchoose: ";
     int choose;
     cin >> choose;
@@ -147,7 +153,7 @@ int main()
         switch (choose)
         {
         case 1:
-            admin_dashboard();
+            obj.admin_dashboard();
             break;
         case 2:
             // seller_dashboard();
