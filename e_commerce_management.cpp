256721b4fa7a0f9cfca71a38bb9536ce258b4da5
Fazle-Rabbi-Rounok <149162051+Fazle-Rabbi-Rounok@users.noreply.github.com>
2024-10-01 12:06:23 +0600

    Update e_commerce_management.cpp

--- e_commerce_management.cpp
+++ e_commerce_management.cpp
@@ -1,14 +1,76 @@
 #include <bits/stdc++.h>
 using namespace std;
+vector<seller> seller_list;
+vector<customer> customer_list;
+vector<purchase> purchase_hstry;
+vector<products> all_prod;
+
 class Rshop
 {
-    string log_in = "admin";
+    string user_name = "admin";
     string log_pass = "admin123";
-    void remove_seller()
+
+public:
+    void admin_dashboard()
+    {
+        string user, logpass;
+        cin.ignore();
+        cout << "please log in:\nenter user name: ";
+        getline(cin, user);
+        cout << "enter password: ";
+        getline(cin, logpass);
+        if (user_name == user && log_pass == logpass)
+            dashboard();
+        else
+            cout << "wrong username or password\n";
+    }
+    void dashboard()
+    {
+        Rshop obj;
+        cout << "welcome to admin dashboar:  \n\nplease choose any option given below: \n1.view sellers\n2.view customers\n3. ban someone\n4.exit\nchoose: ";
+        int choose;
+        cin >> choose;
+        while (choose != 4)
+        {
+            switch (choose)
+            {
+            case 1:
+                obj.view_seller();
+                break;
+            case 2:
+                obj.view_customers();
+                break;
+            case 3:
+                obj.ban_();
+                break;
+            default:
+                cout << "invalid input\n ";
+                break;
+            }
+            cout << "1.view sellers\n2.view customers\n3. ban someone\n4.exit\nchoose: ";
+            cin >> choose;
+        }
+    }
+    void view_seller()
+    {
+        for (auto &a : seller_list)
+        {
+        }
+    }
+    void view_customers() {}
+    void ban_() {}
+    void ban_seller()
     {
     }
+    void ban_customer() {}
+};
+class seller : public Rshop
+{
 
-protected:
+    string log_in;
+    string log_pass;
+
+public:
     void add_product()
     {
     }
@@ -24,11 +86,6 @@ protected:
     void remove_product()
     {
     }
-};
-class seller : public Rshop
-{
-    string log_in;
-    string log_pass;
 };
 class customer
 {
@@ -52,6 +109,32 @@ public:
     {
     }
 };
+class purchase;
+class products;
 int main()
 {
+    Rshop admin_obj;
+    cout << "1.log in as admin\n2.log in as a seller\n3.log in as customer\nchoose: ";
+    int choose;
+    cin >> choose;
+    while (choose != 4)
+    {
+        switch (choose)
+        {
+        case 1:
+            admin_obj.admin_dashboard();
+            break;
+        case 2:
+            // seller_dashboard();
+            break;
+        case 3:
+            //  customer_dashboard();
+            break;
+        default:
+            cout << "invalid input\n ";
+            break;
+        }
+        cout << "1.log in as admin\n2.log in as a seller\n3.log in as customer\nchoose: ";
+        cin >> choose;
+    }
 }
